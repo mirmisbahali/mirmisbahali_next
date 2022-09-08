36d2479a1ffafd54b38ebf8b1d095d6bc70c16e8
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-09-08 19:43:31 +0530

    fixed js

--- pages/_document.js
+++ pages/_document.js
@@ -19,9 +19,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" />
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js
-" type="" />
+        
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" ></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="assets/js/script.js" ></script>
       </body>
     </Html>
   );
