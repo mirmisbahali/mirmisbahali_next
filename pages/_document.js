ac7b6e4a0cb9bc27d6d0ba1334006629e855392a
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-10-15 12:19:17 +0530

    Fixed google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -15,13 +15,19 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
         <link href="css/styles.css" rel="stylesheet" />
+        <Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5XFHMMF');`}}></Script>
 
       </Head>
       <body>
-
+<noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XFHMMF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
-        <Script src="assets/js/scripts.js"></Script>
+        <Script strategy="afterInteractive" src="assets/js/scripts.js"></Script>
         
       </body>
     </Html>
