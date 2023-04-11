cc4bfff11c51cd8d72c5ac055a46955cbc930fea
Mir Misbah Ali <mirmisbahali@hotmail.com>
2023-04-11 17:20:48 +0530

    added profile photo

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,9 +7,8 @@ const Navbar = () => {
       <a className="navbar-brand js-scroll-trigger" href="#page-top">
         <span className="d-block d-lg-none">Mir Misbah Ali</span>
         <span className="d-none d-lg-block">
-          {/* <img className="img-fluid img-profile rounded-circle mx-auto mb-2"
-          src="assets/img/profile.jpg" alt="..." /> */}
-          Mir Misbah Ali
+          <img className="img-fluid img-profile rounded-circle mx-auto mb-2"
+          src="assets/img/myprofile.jpg" alt="..." />
         </span>
       </a>
       <button
